3a40cd6ebc21019cd0802e274d23d0a8ac217ab5
Gaurav Singh <120627440+gaurav-singh20@users.noreply.github.com>
2024-06-27 21:35:37 +0530

    footer added and few changes

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,8 +3,10 @@ import './Footer.css'
 export default function Footer(){
     return(
         <div className="footer_container">
-            <div className="footer_text">
-            <p>&copy; 2024 React Project by me. </p>
+            <div className="footer_text_left">
+                <p>Copyright &copy;2024 React Project by <span className='footer-name'>Gaurav Singh</span>. </p>
+            </div>
+            <div className="footer_text_right">
             <p>Not an actual Portfolio. Just built it as a project using React+Vite.</p>
             </div>
         </div>
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -11,7 +11,18 @@ const container_x = (d) => ({
         opacity: 1,
         transition: {
             delay: d,
-            duration: 0.3
+            duration: 0.5
+        }
+    }
+})
+const container_y = (d) => ({
+    hidden: { x:100,opacity: 0 },
+    visible: {
+        x: 0,
+        opacity: 1,
+        transition: {
+            delay: d,
+            duration: 0.8
         }
     }
 })
@@ -27,13 +38,13 @@ function Projects() {
     return <div className='project_box'>
         <motion.p 
         ref={ref}
-        variants={container_x(0.4)}
+        variants={container_x(0.2)}
         initial='hidden'
         animate={inView ? 'visible' : 'hidden'}
         className="projects_title">Projects</motion.p>
         <motion.div 
         ref={ref}
-        variants={container_x(0.2)}
+        variants={container_y(0.7)}
         initial='hidden'
         animate={inView ? 'visible' : 'hidden'} className='projects_box_container'>
 
